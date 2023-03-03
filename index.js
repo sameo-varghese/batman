428c560d30a090fb4e00649fcc38861026d121bf
sameo-varghese <122598713+sameo-varghese@users.noreply.github.com>
2023-03-03 15:33:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+// let i=1;
+// console.log(i);
+// ++i;
+// console.log(i);
+// ++i;
+// console.log(i);
+// ++i;
+// console.log(i);
+// ++i;
+// console.log(i);
+
+// for (let i=1;i<=10 ; i++)
+//     {console.log(i);}
+
+
+// let num=[1,2,3,4,5];
+// let sum=0;
+// for (i=0;i< num.length;i++)
+//     {sum=sum+num[i]}
+// console.log(sum)
+
+
+// let time=19;
+// if 
+// (time >=12 , time<=17)
+// {
+//     console.log("Good evening")
+//     ;}
+// else if
+// (time >=1 , time<=11)
+// {
+//     console.log("Good Morning")
+// }
+// else 
+// {
+//     console.log("Good afternoon")
+// }
+// if (time >=1 , time<=11);
+// console.log("Good morning");
+// if (time >=12 , time<=16);
+// console.log("Good evening");
+
+// let fruits =["apple", "orange", "grapes", "banana"]
+// // // for (i=0;i< fruits.length;i++)
+// // // {
+// // // console.log(fruits[i])}
+// // let i=0;
+// // while( i<fruits.length)
+// // {console.log(fruits[i]);
+// // i++};
+// let i=0
+// do{ console.log(fruits[i]);i++;}
+// while (i<fruits.length)
+
+
+
+
+
+// let num=20;
+// if (num%2==0)
+// {console.log("It's a even number !!")}
+// else{console.log("It's a odd number !!")}
+
+
+let p=[2,3,4,6,7]
+
+for (i=0;i<p.length;i++){
+if (p[i]%2==0)
+{console.log(p[i],"'s a even number !!")}
+else{console.log(p[i],"'s a odd number !!")}}
+
